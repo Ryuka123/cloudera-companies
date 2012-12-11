3b3ae23efc255945ae5fff84d63fc2f7bca8618a
ggear <graham.gear@gmail.com>
2012-12-11 11:03:53 +0000

    add shel launche scripts

--- com.cloudera.companies.core/com.cloudera.companies.core.assembly/src/main/assembly/layout/bin/companies_fsck.sh
+++ com.cloudera.companies.core/com.cloudera.companies.core.assembly/src/main/assembly/layout/bin/companies_fsck.sh
@@ -2,5 +2,5 @@
 
 source companies.env
 
-hadoop jar "../lib/ingest/com.cloudera.companies.core.ingest-*-hadoop-job.jar" com.cloudera.companies.core.ingest.IngestFSCKDriver "$COMPANIES_EXPORT_DATA_ZIP_DIR" "$COMPANIES_EXPORT_DATA_SEQ_DIR"
+hadoop jar "$COMPANIES_DIR/lib/ingest/com.cloudera.companies.core.ingest-*-hadoop-job.jar" com.cloudera.companies.core.ingest.IngestFSCKDriver "$COMPANIES_EXPORT_DATA_ZIP_DIR" "$COMPANIES_EXPORT_DATA_SEQ_DIR"
 
--- com.cloudera.companies.core/com.cloudera.companies.core.assembly/src/main/assembly/layout/bin/companies_ingest.sh
+++ com.cloudera.companies.core/com.cloudera.companies.core.assembly/src/main/assembly/layout/bin/companies_ingest.sh
@@ -2,5 +2,5 @@
 
 source companies.env
 
-hadoop jar "../lib/ingest/com.cloudera.companies.core.ingest-*-hadoop-job.jar" com.cloudera.companies.core.ingest.IngestDriver "$COMPANIES_DATA_DIR" "$COMPANIES_EXPORT_DATA_ZIP_DIR" "$COMPANIES_EXPORT_DATA_SEQ_DIR"
+hadoop jar "$COMPANIES_DIR/lib/ingest/com.cloudera.companies.core.ingest-*-hadoop-job.jar" com.cloudera.companies.core.ingest.IngestDriver "$COMPANIES_DATA_DIR" "$COMPANIES_EXPORT_DATA_ZIP_DIR" "$COMPANIES_EXPORT_DATA_SEQ_DIR"
 
