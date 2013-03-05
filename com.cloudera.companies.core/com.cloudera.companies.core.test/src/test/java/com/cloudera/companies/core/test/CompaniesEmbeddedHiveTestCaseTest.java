aff22abcdaaccfa581bfeafd0918c5a301939eb1
ggear <graham.gear@gmail.com>
2013-03-05 12:57:57 +0000

    add some simple tests to unit test framework

--- com.cloudera.companies.core/com.cloudera.companies.core.test/src/test/java/com/cloudera/companies/core/test/CompaniesEmbeddedHiveTestCaseTest.java
+++ com.cloudera.companies.core/com.cloudera.companies.core.test/src/test/java/com/cloudera/companies/core/test/CompaniesEmbeddedHiveTestCaseTest.java
@@ -5,6 +5,8 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
+import junit.framework.Assert;
+
 public class CompaniesEmbeddedHiveTestCaseTest extends CompaniesEmbeddedHiveTestCase {
 
   public CompaniesEmbeddedHiveTestCaseTest() throws IOException {
@@ -18,14 +20,17 @@ public class CompaniesEmbeddedHiveTestCaseTest extends CompaniesEmbeddedHiveTest
 
     BufferedWriter writer = new BufferedWriter(new FileWriter(localDataFile));
     writer.write("1,1\n");
-    writer.write("2,1\n");
+    writer.write("2,2\n");
+    writer.write("3,3\n");
     writer.close();
 
-    hive.execute("create table  somedata  (col1 int, col2 int)");
-    hive.execute("load data local inpath '" + localDataFile.toString() + "' into table somedata");
+    hive.execute("create table if not exists somedata  (col1 int, col2 int) row format delimited fields terminated by ',' stored as textfile");
+    hive.execute("load data local inpath '" + localDataFile.toString() + "' overwrite into table somedata");
     hive.execute("select count(1) as cnt from somedata");
-    String row = hive.fetchOne();
-    assertEquals(row, "2");
+    Assert.assertEquals("3", hive.fetchOne());
+    hive.execute("select col1 from somedata where col2 = 2");
+    Assert.assertEquals("2", hive.fetchOne());
     hive.execute("drop table somedata");
   }
+
 }
